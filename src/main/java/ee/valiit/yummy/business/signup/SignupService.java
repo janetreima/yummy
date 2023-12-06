0bf2f5030c0970854d5d2f04cba607a09f595c1d
Evgeny <epsilon1293@gmail.com>
2023-12-06 11:39:28 +0200

    Registration done

--- src/main/java/ee/valiit/yummy/business/signup/SignupService.java
+++ src/main/java/ee/valiit/yummy/business/signup/SignupService.java
@@ -26,23 +26,12 @@ public class SignupService {
 
     public void signUp(UserInfoDto userInfoDto) {
 
-        System.out.println("we are in signUp service ");
-
-
         User user = userMapper.userFromUserDto(userInfoDto);    //sozdal User user 1)
-        System.out.println("Created new user " + user);
-
         user.setRole(new Role(2, "user"));   //  2) prinuditeljno zahardkodil Role=2
         userService.saveUser(user);                //  3) sohranjaju v tablicu user
-        System.out.println("saved user " + user);
-
         Profile profile = profileMapper.profileFromUserDto(userInfoDto);
-        System.out.println("profile: " + profile);
         profile.setUser(user);
-        System.out.println("profile: " + profile);
-
         profileService.saveProfile(profile);
-        System.out.println("saved profile " + profile);
     }
 
 }
