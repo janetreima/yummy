a61f3c08e3aeeacdaf134a8efed02e4f0cd26623
ViM <vitaliimakarenkov@gmail.com>
2023-12-06 09:45:41 +0200

    ups

--- src/main/java/ee/valiit/yummy/domain/image/Image.java
+++ src/main/java/ee/valiit/yummy/domain/image/Image.java
@@ -1,26 +1,24 @@
-package ee.valiit.yummy.domain.recipe;
+package ee.valiit.yummy.domain.image;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "course", schema = "project")
-public class Course {
+@Table(name = "image", schema = "project")
+public class Image {
     @Id
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Size(max = 255)
     @NotNull
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Column(name = "data", nullable = false)
+    private byte[] data;
 
 }
